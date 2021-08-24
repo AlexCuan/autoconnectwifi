7307c12a95e08809fdd623ad56564738d25dd51e
fav09 <fav09@protonmail.com>
2021-08-24 15:04:54 -0400

    fix some bugs

--- autoconnect_poo.py
+++ autoconnect_poo.py
@@ -15,10 +15,28 @@ class Reconnect(object):
         """
         Keeps sending ping and returning a boolean value
         """
-        if ping(self.hostname):
+        counter = 0
+        for i in range(2):
+            r = ping(self.hostname)
+            print("Ping to: " + self.hostname + " -> Response time: ", r)
+            time.sleep(1)
+
+            if r:
+                counter += 1
+
+        if counter == 2:
             return True
         else:
             return False
+
+    def reconnect(self):
+        print('Desconectado del WIFI ...')
+        print('Ejecutando reconexión en WIFI ...')
+        # if self.SO == 'nt':
+        os.system('netsh wlan connect name="' + self.network_name + '" interface="' + self.interface_name + '"')
+        # elif self.SO == 'posix':
+        #     os.system()
+        # Need some linux programmer who helps me with connection stuff on linux
 
     def stay_connected(self):
         """
@@ -26,22 +44,15 @@ class Reconnect(object):
         """
         while self.is_connected():
             print("You are connected")
-            time.sleep(2)
+            time.sleep(1)
 
         else:
-            print('Desconectado del WIFI ...')
-            print('Ejecutando reconexión en WIFI ...')
-            if self.SO == 'nt':
-                os.system('netsh wlan connect name="' + self.network_name + '" interface="' + self.interface_name + '"')
-            # elif self.SO == 'posix':
-            #     os.system()
-            # Need some linux programmer who helps me with connection stuff on linux
-
-            time.sleep(1)
+            self.reconnect()
+            time.sleep(1.5)
             self.stay_connected()
 
 
-r = Reconnect("10.40.46.4", "Router", "Wi-Fi") # each one instance Reconnect class with his own connection values
+r = Reconnect("192.168.43.1", "Alquenda", "Wi-Fi")  # each one instances Reconnect class with his own connection values
 
 if __name__ == "__main__":
     r.stay_connected()
