a48fe6dd4e921646ebc519cf3647ad75ac64a3b8
fav09 <fav09@protonmail.com>
2021-08-24 13:33:29 -0400

    documentation included

--- autoconnect_poo.py
+++ autoconnect_poo.py
@@ -7,17 +7,23 @@ class Reconnect(object):
     SO = os.name
 
     def __init__(self, hostname, wifi_name, interface):
-        self.hostname = hostname
+        self.hostname = hostname  # router's ip
         self.network_name = wifi_name
-        self.interface_name = interface
+        self.interface_name = interface  # Wi-Fi, Ethernet, etc
 
     def is_connected(self):
+        """
+        Keeps sending ping and returning a boolean value
+        """
         if ping(self.hostname):
             return True
         else:
             return False
 
     def stay_connected(self):
+        """
+        Information about connection state and reconnection
+        """
         while self.is_connected():
             print("You are connected")
             time.sleep(2)
@@ -27,8 +33,9 @@ class Reconnect(object):
             print('Ejecutando reconexión en WIFI ...')
             if self.SO == 'nt':
                 os.system('netsh wlan connect name="' + self.network_name + '" interface="' + self.interface_name + '"')
-            elif self.SO == 'posix':
-                os.system()
+            # elif self.SO == 'posix':
+            #     os.system()
+            # Need some linux programmer who helps me with connection stuff on linux
 
             time.sleep(1)
             self.stay_connected()
